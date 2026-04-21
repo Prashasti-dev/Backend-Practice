4dc87737b9478df6001e8699f84bfbe7ff1a39a6
Prashasti Jaiswal <prashasti.2024@gmail.com>
2026-04-22 01:49:21 +0530

    initial commit

---
+++ Day1/app.js
@@ -0,0 +1,5 @@
+ const express=require("express")
+
+//  call express
+const app=express()        //server create kr chuke hain . It has created server's instance and saved it in app constant
+app.listen(3000)            //start server
