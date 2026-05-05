8df6d4a34fae8328568cb55423232c0e6f38ca5a
Prashasti Jaiswal <prashasti.2024@gmail.com>
2026-05-05 22:54:02 +0530

    API integration

--- day8-Fullstack/Frontend/src/App.jsx
+++ day8-Fullstack/Frontend/src/App.jsx
@@ -1,4 +1,4 @@
-import {  use, useState } from 'react'
+import {   useEffect, useState } from 'react'
 
 
 import React from 'react'
@@ -14,25 +14,90 @@ const [notes, setNotes] = useState([
 
 ]);
 
+//GET API
+const fetchNotes=()=>{
 axios.get('http://localhost:3000/api/notes')
 .then((res=>{
  setNotes(res.data.note)
   
 }))
-  return (
-    <div className='notes'>
-     {
-      notes.map(note=>{
-        return  <div className="note">
-          <h1>{note.title}</h1>
-        <p>{note.description}</p> <br />
-      </div>
-      })
-     }
-     
-    
-    </div>
-  )
 }
+
+useEffect(()=>{
+  fetchNotes()
+},[])
+
+
+//POST api
+const prevent = (e) => {
+  e.preventDefault();
+
+  const {title,description}=e.target.elements
+  console.log(title.value,description.value)
+
+  axios.post("http://localhost:3000/api/notes",{
+     title:title.value,
+  description:description.value
+  })
+  .then(res=>{
+    console.log(res.data)
+    fetchNotes()
+  })
+};
+
+
+//DELETE api
+const delNotes=(noteId)=>{
+console.log(noteId)
+axios.delete("http://localhost:3000/api/notes/"+noteId)
+.then(res=>{
+  console.log(res.data)
+  fetchNotes()
+})
+}
+
+
+//PATCH api
+const modifyNotes=(noteId)=>{
+  const newtitle=prompt("new title")
+  const newdesc=prompt("new description")
+
+console.log(noteId)
+axios.patch("http://localhost:3000/api/notes/"+noteId, {
+  title:newtitle,
+  description: newdesc
+  })
+
+.then(res=>{
+  console.log(res.data)
+  fetchNotes()
+})
+}
+  return (
+  <>
+    <form className='note-create-form' onSubmit={prevent}>
+      <input name='title' type="text" placeholder='enter title' />
+            <input  name='description' type="text" placeholder='enter description' />
+            <button>Create note</button>
+
+    </form>
+
+    <div className='notes'>
+      {
+        notes.map((note, index) => {
+          return (
+            <div className="note" key={index}>
+              <h1>{note.title}</h1>
+              <p>{note.description}</p>
+              <button  onClick={()=>{delNotes(note._id)}}>Delete Note</button>
+              <button  onClick={()=>{modifyNotes(note._id)}}>Modify Note</button>
+
+            </div>
+          )
+        })
+      }
+    </div>
+  </>
+)}
 
 export default App
